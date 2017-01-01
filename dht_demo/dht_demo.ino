5cfda5f6a38e757e9612d2122a7951af2843c396
danm <writetodan@yahoo.com>
2017-01-01 17:54:36 +0000

    @damned try out humidity monitor

---
+++ dht_demo/dht_demo.ino
@@ -0,0 +1,26 @@
+/// @dir dht_demo
+/// Demo readout of the DHT11 or DHT22 temperature/humidity sensors.
+/// @see http://jeelabs.org/2012/04/14/reading-out-dhtxx-sensors/
+// 2012-04-02 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+
+#include <JeeLib.h>
+
+DHTxx dht (6); // connected to DIO3
+
+void setup () {
+  Serial.begin(57600);
+  Serial.println("\n[dht_demo]");
+}
+
+void loop () {
+  int t, h;
+  if (dht.reading(t, h, true)) {
+    Serial.print("temperature = ");
+    Serial.println(t);
+    Serial.print("humidity = ");
+    Serial.println(h);
+    Serial.println();
+  }
+  Serial.println("-");
+  delay(3000);
+}
